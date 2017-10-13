9c0a215adf4f1e8f0b7537babcb72ec56e53fba9
Peter Forrest <petergforrest91@gmail.com>
2017-10-13 12:14:29 -0400

    c# practice

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewQuestion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<string> StringList = new List<string>();
+            Survey(StringList, 8);
+          
+            void Survey(List<string> StrgList, int testInt)
+            {
+                testInt = 8;
+                string[] StrList = {"Hello", "Goodbye", "MoreThanEight"};
+
+                foreach (var j in StrList)
+                {
+                    if(j.Length < testInt)
+                    {
+                        Console.WriteLine(string.Join(" ", j));
+                    }
+                }
+                
+            }
+            var myNumber= 12345;
+            string numberToString = myNumber.ToString();
+            var charArray = numberToString.ToCharArray();
+            for ( var g = 0; g < charArray.Length; g++)
+            {
+                Console.WriteLine(charArray[g]);
+            } 
+        }
+    }
+}
+
